7489c6f130e4e2a78679e0d67c557147293bd7c6
chenfangyuan <15929720704@163.com>
2018-04-16 15:12:31 +0800

    my app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    App
+  </div>
+</template>
+
+<script>
+  /*1.引入*/
+ /* import FootGuide from './components/FooterGuide/FooterGuide.vue'*/
+
+  export default {
+    /*2.映射为标签*/
+   /* components: {
+      FooterGuide
+    }*/
+  }
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+
+import Vue from 'vue'
+import App from './App.vue'
+
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+ /* router //注册上路由器（路由相关语法才能使用）*/
+})
